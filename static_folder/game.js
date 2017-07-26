2310e799d9f7ca3bfe6862563db8fcfef2db4ca9
Demo User <demouser@MacBook-Air.local>
2017-07-26 09:19:28 -0400

    This is the updated version of my blog and contains all the different files in their respective folders

--- static_folder/game.js
+++ static_folder/game.js
@@ -3,12 +3,15 @@ function randomNum() {
 }
 
 function setup() {
-  $("#ok_button").click(greeting);
+  $("#ok_button1").click(greeting);
 }
 
 function greeting() {
   var userName = $('#username').val();
   alert("Hi " + userName + ",\n You is kind\n You is smart\n You is strong \nGood Luck in this game!");
+  var header = $('h1');
+  header.text("Welcome to my Number Guessing Game" + userName);
 }
+
 
 $(document).ready(setup);
